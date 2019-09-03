23410ce6984fe0fbf28a66288b9804fea824e3b5
Mikhail Andreev <tundraray@outlook.com>
2019-09-04 00:23:35 +0300

    fix cli tests

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,12 +1,34 @@
+
 import cli from './cli';
 
 
 describe('This is the tests for the workspaces command', () => {
+  let mockLog: any = null;
+  let mockError: any = null;
+  let mockExit: any = null;
+  beforeAll(() => {
+    mockError = jest.spyOn(global.console, 'error').mockImplementation();
+    mockLog = jest.spyOn(global.console, 'log').mockImplementation();
+    mockExit = jest.spyOn(process, 'exit').mockImplementation();
+  });
+
+  afterAll(() => {
+    mockLog.mockRestore();
+    mockExit.mockRestore();
+    mockError.mockRestore();
+  });
+
+
   test('cli since master', async () => {
     await cli(['workspaces', '--since=master']);
   });
 
   test('cli since master', async () => {
     await cli(['run', 'test', '--since=master']);
+  });
+
+  test('cli since master', async () => {
+    await cli(['ororo']);
+    expect(mockExit).toHaveBeenCalledWith(0);
   });
 });
