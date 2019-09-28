685315ffb8575b2eaae38b74dfd382155b0ec576
Mikhail Andreev <mikhaila@123software.ru>
2019-09-28 15:26:11 +0300

    feat: git add changed files, commit and push

--- src/commands/versions.ts
+++ src/commands/versions.ts
@@ -1,83 +1,113 @@
 /* eslint-disable @typescript-eslint/promise-function-async */
+/* eslint-disable @typescript-eslint/unbound-method */
 import chalk from 'chalk';
+import log from 'npmlog';
+import os from 'os';
 import {
   getWorkspaces,
   toWorkspacesRunOptions,
   updateWorkspaceConfig,
 } from '../utils/workspaces';
-import { addTag, pushTag } from '../utils/git';
 import { getNextVersionsByWorkspaces } from '../utils/versions';
 import {
-  trimmedColumns,
-  write,
+  outputFormat,
+  gitCommit,
+  gitAdd,
+  addTag,
+  gitPush,
 } from '../utils';
 import {
   IFlags, IWorkspaceVersion,
 } from '../interfaces';
+import { IPackagePrint } from './workspaces';
 
 
-function addWorkspaceTag(enable: boolean) {
-  return async function func(
+function updateWorkspaceVersion(enable: boolean) {
+  return function func(
     {
       workspace,
       nextVersion,
     }: IWorkspaceVersion,
   ): Promise<IWorkspaceVersion> {
+    let chain = Promise.resolve();
     if (enable) {
-      const tag = nextVersion ? `${workspace.getName()}-v${nextVersion}` : null;
-      await addTag(tag);
+      const { config: { filePath } } = workspace;
+      chain = chain.then(() => updateWorkspaceConfig(workspace, { version: nextVersion }));
+      chain = chain.then(() => gitAdd([filePath]));
     }
-    return { workspace, nextVersion };
+
+    return chain.then(() => ({ workspace, nextVersion }));
   };
 }
 
-function updateWorkspaceVersion(enable: boolean) {
-  return async function func(
+function gitCommitAndTagVersion(
+  workspaces: IWorkspaceVersion[],
+  { message: subject = 'Publish', gitTagVersion, ...flags }: IFlags,
+): Promise<IWorkspaceVersion[]> {
+  const tags = workspaces.map((
     {
       workspace,
       nextVersion,
-    }: IWorkspaceVersion,
-  ): Promise<IWorkspaceVersion> {
-    if (enable) {
-      await updateWorkspaceConfig(workspace, { version: nextVersion });
-    }
-    return { workspace, nextVersion };
-  };
+    },
+  ) => `${workspace.getName()}-v${nextVersion}`);
+
+  const message = tags.reduce((msg, tag) => `${msg}${os.EOL} - ${tag}`, `${subject}${os.EOL}`);
+
+  return Promise.resolve()
+    .then(() => gitCommit(message, flags))
+    .then(() => gitTagVersion && Promise.all(tags.map(tag => addTag(tag))))
+    .then(() => workspaces);
+}
+
+function outputCommand(
+  workspaces: IWorkspaceVersion[],
+  flags: IFlags,
+): IWorkspaceVersion[] {
+  const workspacesToPrint = workspaces
+    .map((
+      {
+        workspace,
+        nextVersion,
+      }: IWorkspaceVersion,
+    ): IPackagePrint => {
+      const { config: { json: { description, private: $private } } } = workspace;
+      return {
+        name: workspace.getName(),
+        version: chalk.green(nextVersion || 'No next version'),
+        description,
+        private: $private,
+        location: workspace.dir,
+      };
+    });
+
+  outputFormat(workspacesToPrint, flags);
+  return workspaces;
+}
+
+function gitPushToRemote({
+  gitRemote = 'origin',
+  branch = 'master',
+  push,
+}: IFlags): Promise<any> {
+  log.info('git', 'Pushing tags...');
+
+  return push && gitPush(gitRemote, branch);
 }
 
 
 export async function commandGetVersionsByWorkspaces(
   _args: string[],
-  {
-    push,
-    gitTagVersion,
-    ...flags
-  }: IFlags,
-): Promise<void> {
-  try {
-    const opts = toWorkspacesRunOptions(_args, flags);
-    const workspaces = await getWorkspaces(opts.filterOpts);
-    const versionsByWorkspace: IWorkspaceVersion[] = await getNextVersionsByWorkspaces(workspaces);
+  flags: IFlags,
+): Promise<any> {
+  const opts = toWorkspacesRunOptions(_args, flags);
 
-    const promises = versionsByWorkspace
-      .filter(({ nextVersion }) => nextVersion !== null)
-      .map(workspaceVersion => Promise.resolve(workspaceVersion)
-        .then(addWorkspaceTag(gitTagVersion))
-        .then(updateWorkspaceVersion(true)));
-
-    await (Promise.all(promises).then(() => (push ? pushTag() : null)));
-
-
-    const versionsToPrint: { workspace: string; version: string | null }[] = [];
-    versionsByWorkspace.forEach(({ workspace, nextVersion }: IWorkspaceVersion): void => {
-      versionsToPrint.push({
-        workspace: workspace.getName(),
-        version: chalk.green(nextVersion || 'No next version'),
-      });
-    });
-
-    write(trimmedColumns(versionsToPrint, ['workspace', 'version']));
-  } catch (error) {
-    console.error(error);
-  }
+  return getWorkspaces(opts.filterOpts)
+    .then(getNextVersionsByWorkspaces)
+    .then(workspaces => workspaces.filter(({ nextVersion }) => nextVersion !== null))
+    .then(workspaces => workspaces.map(workspace => Promise.resolve(workspace)
+      .then(updateWorkspaceVersion(true))))
+    .then(Promise.all)
+    .then((workspaces: IWorkspaceVersion[]) => gitCommitAndTagVersion(workspaces, flags))
+    .then(workspaces => outputCommand(workspaces, flags))
+    .then(() => gitPushToRemote(flags));
 }
